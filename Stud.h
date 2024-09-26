905cfff02aba19812be9353ee700a45d13599290
AKrisciunas <andrius1krisciunas@gmail.com>
2024-09-26 12:56:21 +0300

    restoring files after accidental deletion

---
+++ Stud.h
@@ -0,0 +1,20 @@
+#ifndef STUD_H_INCLUDED
+#define STUD_H_INCLUDED
+
+#include "Mylib.h"
+
+struct Stud
+{
+    string name, surname;
+    vector<int> nd;
+    int egz;
+};
+
+void input(Stud &Local, int n);
+void inputRandom(Stud &Local, int n);
+void output(vector<Stud> vec, string method);
+void empty(Stud &Local);
+double averageResult(vector<int> nd, int egz);
+double medianResult(vector<int> nd, int egz);
+
+#endif
