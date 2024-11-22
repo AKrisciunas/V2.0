f90e085dc64f9bd8e9995557223ad6f1074f6413
AKrisciunas <andrius1krisciunas@gmail.com>
2024-11-22 19:51:27 +0200

    src files

---
+++ src/Mylib.h
@@ -0,0 +1,20 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <random>
+#include <list>
+
+using std::cout;
+using std::cin;
+using std::setw;
+using std::string;
+using std::vector;
+using std::list;
+using std::getline;
+
+#endif
