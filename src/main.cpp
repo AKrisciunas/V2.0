206b25286337c973438afe2b13117147f375827c
AKrisciunas <andrius1krisciunas@gmail.com>
2024-12-14 14:43:59 +0200

    main.cpp updated for class changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,28 +6,27 @@
 
 int globalND=0;
 
-
 template<typename Container>
 void sortContainer(Container& A, char sortType, string method) {
     if (sortType == '0') {
         if (method == "Vid.") {
             if constexpr (std::is_same_v<Container, std::vector<Stud>>) {
-                std::sort(A.begin(), A.end(), [](const Stud& a, const Stud& b) {
-                    return averageResult(a.nd, a.egz) < averageResult(b.nd, b.egz);
+                std::sort(A.begin(), A.end(), []( Stud& a,  Stud& b) {
+                    return averageResult(a.nd(), a.egz()) < averageResult(b.nd(), b.egz());
                 });
             } else if constexpr (std::is_same_v<Container, std::list<Stud>>) {
-                A.sort([](const Stud& a, const Stud& b) {
-                    return averageResult(a.nd, a.egz) < averageResult(b.nd, b.egz);
+                A.sort([]( Stud& a, Stud& b) {
+                    return averageResult(a.nd(), a.egz()) < averageResult(b.nd(), b.egz());
                 });
             }
         } else if (method == "Med.") {
             if constexpr (std::is_same_v<Container, std::vector<Stud>>) {
-                std::sort(A.begin(), A.end(), [](const Stud& a, const Stud& b) {
-                    return medianResult(a.nd, a.egz) < medianResult(b.nd, b.egz);
+                std::sort(A.begin(), A.end(), []( Stud& a,  Stud& b) {
+                    return medianResult(a.nd(), a.egz()) < medianResult(b.nd(), b.egz());
                 });
             } else if constexpr (std::is_same_v<Container, std::list<Stud>>) {
-                A.sort([](const Stud& a, const Stud& b) {
-                    return medianResult(a.nd, a.egz) < medianResult(b.nd, b.egz);
+                A.sort([]( Stud& a,  Stud& b) {
+                    return medianResult(a.nd(), a.egz()) < medianResult(b.nd(), b.egz());
                 });
             }
         }
@@ -70,16 +69,20 @@ void readFile(Container& A, string fileName){
             std::istringstream iss(line);
             Stud S;
 
-            iss >> S.name >> S.surname;
+            string name, surname;
+            iss >> name >> surname;
+            S.setName(name);
+            S.setSurname(surname);
 
             int temp;
             for (int i = 0; i < globalND; i++) {
                 if (iss >> temp) {
-                    S.nd.push_back(temp);
+                    S.nd().push_back(temp);
                 }
             }
 
-            iss >> S.egz;
+            iss >> temp;
+            S.setEgz(temp);
             A.push_back(S);
         }
         input.close();
@@ -90,15 +93,15 @@ void readFile(Container& A, string fileName){
 }
 
 template<typename Container>
-void writeFile(Container& A, string fileName, int countND) {
+void writeFile( Container& A, string fileName, int countND) {
     std::ostringstream buffer;
     buffer << std::left << setw(15) << "Vardas" << setw(15) << "Pavarde"; 
     for (int i = 0; i < countND; i++) buffer << std::left << setw(5) << "ND" + std::to_string(i+1);
     buffer << "Egz.\n";    
-    for (const auto& stud : A) {
-        buffer << setw(15) << stud.name << setw(15) << stud.surname << setw(5);
-        for (auto i = 0; i < countND; i++)   buffer << stud.nd.at(std::vector<int>::size_type(i)) << setw(5);   
-        buffer << stud.egz << "\n";
+    for ( auto& stud : A) {
+        buffer << setw(15) << stud.name() << setw(15) << stud.surname() << setw(5);
+        for (auto i = 0; i < countND; i++)   buffer << stud.nd().at(std::vector<int>::size_type(i)) << setw(5);   
+        buffer << stud.egz() << "\n";
     }
 
     std::ofstream file(fileName);
@@ -123,13 +126,13 @@ void output(Container &A, string method){
     }
     if(method == "Vid."){
         for (auto &&Stud : A)
-            cout << std::left << setw(20) << &Stud << setw(15) << Stud.name << setw(15) << Stud.surname 
-            << std::fixed << std::setprecision(2) << averageResult(Stud.nd, Stud.egz) <<"\n";
+            cout << std::left << setw(20) << &Stud << setw(15) << Stud.name() << setw(15) << Stud.surname() 
+            << std::fixed << std::setprecision(2) << averageResult(Stud.nd(), Stud.egz()) <<"\n";
     }
     else{
         for (auto &&Stud : A)
-            cout << std::left << setw(20) << &Stud << setw(15) << Stud.name << setw(15) << Stud.surname 
-            << std::fixed << std::setprecision(2) << medianResult(Stud.nd, Stud.egz) << "\n";
+            cout << std::left << setw(20) << &Stud << setw(15) << Stud.name() << setw(15) << Stud.surname() 
+            << std::fixed << std::setprecision(2) << medianResult(Stud.nd(), Stud.egz()) << "\n";
     }
     cout << "-----------------------------------------------------------------\n";
 }
@@ -140,8 +143,8 @@ void splitFileStrat1(Container& A, string method, char sortType){
     Container kietiakai;
     Container vargsiukai;
     if(method == "Vid."){
-        for (const auto& stud : A) {
-            if (averageResult(stud.nd, stud.egz) < 5.0) {
+        for ( auto& stud : A) {
+            if (averageResult(stud.nd(), stud.egz()) < 5.0) {
                 vargsiukai.push_back(stud);
             }
             else {
@@ -150,8 +153,8 @@ void splitFileStrat1(Container& A, string method, char sortType){
         }
     }
     else{
-        for (const auto& stud : A) {
-            if (medianResult(stud.nd, stud.egz) < 5.0) {
+        for ( auto& stud : A) {
+            if (medianResult(stud.nd(), stud.egz()) < 5.0) {
                 vargsiukai.push_back(stud);
             }
             else {
@@ -171,7 +174,7 @@ void splitFileStrat2(Container& A, string method, char sortType) {
     Container vargsiukai;
     for (auto it = A.begin(); it != A.end();) {
         if (method == "Vid.") {
-            if (averageResult(it->nd, it->egz) < 5.0) {
+            if (averageResult(it->nd(), it->egz()) < 5.0) {
                 vargsiukai.push_back(*it);
                 it = A.erase(it);
             } else {
@@ -179,7 +182,7 @@ void splitFileStrat2(Container& A, string method, char sortType) {
             }
         }
         else {
-            if (medianResult(it->nd, it->egz) < 5.0) {
+            if (medianResult(it->nd(), it->egz()) < 5.0) {
                 vargsiukai.push_back(*it);
                 it = A.erase(it);
             } else {
@@ -239,86 +242,63 @@ template<typename Container>
 void userInterface(Container& A){
     Stud S;
     char choice1, choice2;
-    int n=0;
-    cout<<"(0) - Input information yourself \n(1) - Generate random homework and exam points \n(2) - Read data from file\n" 
-    << "(3) - Generate random data to file \n(4) - Split file into 2, based on results\n: ";
-    cin>>choice1;
-    if(choice1 != '3'){
-        cout<<"Use average - (0) or median - (1) for result calculation: ";
-        cin>>choice2;
+    int n = 0;
+    cout << "(0) - Input information yourself \n(1) - Generate random homework and exam points \n(2) - Read data from file\n"
+         << "(3) - Generate random data to file \n(4) - Split file into 2, based on results\n: ";
+    cin >> choice1;
+    if (choice1 != '3') {
+        cout << "Use average - (0) or median - (1) for result calculation: ";
+        cin >> choice2;
     }
     string calculationMethod = "Vid.";
-    if(choice2 == '1')  calculationMethod = "Med.";
+    if (choice2 == '1') calculationMethod = "Med.";
 
-    //If reading data from file, program will find the number of homework results automatically
-    if(choice1 != '2' && choice1 != '4'){
-        cout<<"How much homework results per student: ";  
-        while(n<=0)
-            cin>>n;  
+    if (choice1 != '2' && choice1 != '4') {
+        cout << "How much homework results per student: ";
+        while (n <= 0)
+            cin >> n;
     }
     string fileName;
     int generateCount;
-    if(choice1 == '2'){
-        cout<<"Write file directory: ";
-        cin>>fileName;
+    if (choice1 == '2') {
+        cout << "Write file directory: ";
+        cin >> fileName;
         readFile(A, fileName);
         output(A, calculationMethod);
-    }
-    else if(choice1 == '3'){
-        cout<<"How much students to generate: ";
-        cin>>generateCount;
-        for (int i = 0; i < generateCount; i++)
-        {
-           S.name = "Vardas" + std::to_string(i+1);
-           S.surname = "Pavarde" + std::to_string(i+1);
-           inputRandom(S, n);
-           A.push_back(S);
-           empty(S);
+    } else if (choice1 == '3') {
+        cout << "How much students to generate: ";
+        cin >> generateCount;
+        for (int i = 0; i < generateCount; i++) {
+            std::string name = "Vardas" + std::to_string(i + 1);
+            std::string surname = "Pavarde" + std::to_string(i + 1);
+            std::vector<int> nd;
+            int egz;
+            inputRandom(S, n);
+            S.setName(name);
+            S.setSurname(surname);
+            A.push_back(S);
         }
-        writeFile(A, std::to_string(generateCount)+"studentu.txt", n);
-    }
-    else if(choice1 == '4'){
+        writeFile(A, std::to_string(generateCount) + "studentu.txt", n);
+    } else if (choice1 == '4') {
         char strategy;
-        cout<<"Which strategy to use: \n0 - (original solution) Finding the split point and split into 2 \n1 - Looping through all students and splitting into 2\n"
-        << "2 - adding to new container and removing from original\n: ";
-        cin>>strategy;
-        cout<<"Write file directory: ";
-        cin>>fileName;
-        cout<<"(0) - to sort new files by result, (1) - sort by name and surname or (2) - sort only surname\n:";
+        cout << "Which strategy to use: \n0 - (original solution) Finding the split point and split into 2 \n1 - Looping through all students and splitting into 2\n"
+             << "2 - adding to new container and removing from original\n: ";
+        cin >> strategy;
+        cout << "Write file directory: ";
+        cin >> fileName;
+        cout << "(0) - to sort new files by result, (1) - sort by name and surname or (2) - sort only surname\n:";
         char sortType = '0';
         cin >> sortType;
         Timer t;
         readFile(A, fileName);
-        generateCount = (int) A.size();
-        if(strategy == '1'){
+        generateCount = static_cast<int>(A.size());
+        if (strategy == '1') {
             splitFileStrat1(A, calculationMethod, sortType);
-        }else if (strategy == '2'){
+        } else if (strategy == '2') {
             splitFileStrat2(A, calculationMethod, sortType);
-        }else{
-            splitFile(A, calculationMethod, generateCount, sortType);   
+        } else {
+            splitFile(A, calculationMethod, generateCount, sortType);
         }
-        cout << " entries whole test time: " << t.elapsed() << "\n";
-    }
-    else{
-        char end;
-        while (true){
-            cout<<"Input Name: ";
-            cin >> S.name;
-            cout <<"Surname: ";
-            cin >> S.surname;
-            if(choice1 == '1')
-                inputRandom(S, n);
-            else 
-                input(S, n);
-            A.push_back(S);  
-            empty(S); 
-            cout<<"Type (0) if you wish to stop, type (1) to continue: ";
-            cin>>end;
-            if(end == '0'){
-                break;
-            } 
-        }
-        output(A, calculationMethod);
     }
 }
 
@@ -340,4 +320,3 @@ int main()
 
     return 0;   
 }
-
